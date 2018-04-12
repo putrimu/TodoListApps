9a0824f7538abe1e36edc89cf8e463a8b89305c9
putrimu <putrimariahulfa@gmail.com>
2018-04-12 17:52:15 +0700

    editing gradle (version)

--- app/src/main/java/mu/android/belajar/todolistapps/activity/MainActivity.java
+++ app/src/main/java/mu/android/belajar/todolistapps/activity/MainActivity.java
@@ -6,25 +6,23 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.nikko.todolistapps.R;
-import com.example.nikko.todolistapps.db.TaskContract;
-import com.example.nikko.todolistapps.db.TaskDBHelper;
+import mu.android.belajar.todolistapps.R;
+import mu.android.belajar.todolistapps.db.TaskContract;
+import mu.android.belajar.todolistapps.db.TaskDBHelper;
 
 public class MainActivity extends AppCompatActivity {
 
